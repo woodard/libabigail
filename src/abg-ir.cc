010f88d1994ccc1fbd1fcf397d741e560c6f4469
Dodji Seketeli <dodji@seketeli.org>
2013-03-01 00:38:28 +0100

    Create and expand locations
    	* src/abg-ir.h (class {location, location_manager, decl,
    	scoped_decl, type_base, type_decl, scope_type_decl): Declare new
    	types.
    	* src/abg-ir.cc (class {location, location_manager}): Define new types.

--- src/abg-ir.cc
+++ src/abg-ir.cc
@@ -1,1 +1,146 @@
-#include "abgl-ir.h"
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <iterator>
+
+#include "abg-ir.h"
+
+using std::string;
+
+namespace abigail
+{
+
+location::location()
+  : m_value(0)
+{
+}
+
+location::location (unsigned value)
+  : m_value(value)
+{
+}
+
+/// \brief the location of a token represented in its simplest form.
+/// Instances of this type are to be stored in a sorted vector, so the
+/// type must have proper relational operators.
+class expanded_location
+{
+  string	m_path;
+  unsigned	m_line;
+  unsigned	m_column;
+
+  expanded_location();
+
+public:
+
+  friend class location_manager;
+
+  expanded_location(const string&	path,
+		    unsigned		line,
+		    unsigned		column)
+    : m_path(path), m_line(line), m_column(column)
+  {
+  }
+
+  bool
+  operator==(const expanded_location& l) const
+  {
+    return (m_path == l.m_path
+	    && m_line == l.m_line
+	    && m_column && l.m_column);
+  }
+
+  bool
+  operator<(const expanded_location& l) const
+  {
+    if (m_path < l.m_path)
+      return true;
+    else if (m_path > l.m_path)
+      return false;
+
+    if (m_line < l.m_line)
+      return true;
+    else if (m_line > l.m_line)
+      return false;
+
+    return m_column < l.m_column;
+  }
+};
+
+struct location_manager::priv
+{
+  // This sorted vector contains the expanded locations of the tokens
+  // coming from a given ABI Corpus.  The index of a given expanded
+  // location in the table gives us an integer that is used to build
+  // instance of location types.
+  std::vector<expanded_location> locs;
+};
+
+location_manager::location_manager()
+{
+  m_priv =
+    shared_ptr<location_manager::priv>(new location_manager::priv);
+}
+
+/// Insert the triplet representing a source locus into our internal
+/// vector of location triplet.  Return an instance of location type,
+/// built from an integral type that represents the index of the
+/// source locus triplet into our source locus table.
+///
+/// \param file_path the file path of the source locus
+/// \param line the line number of the source location
+/// \param col the column number of the source location
+location
+location_manager::create_new_location(const std::string&	file_path,
+				      size_t			line,
+				      size_t			col)
+{
+  expanded_location l(file_path, line, col);
+
+  std::vector<expanded_location>::iterator i =
+    std::upper_bound(m_priv->locs.begin(),
+		     m_priv->locs.end(),
+		     l);
+
+  unsigned opaque = 0;
+  if (i == m_priv->locs.end())
+    {
+      m_priv->locs.push_back(l);
+      i = m_priv->locs.end();
+      i--;
+    }
+  else
+    {
+      std::vector<expanded_location>::iterator prev = i;
+      prev--;
+      if (*prev == l)
+	i = prev;
+      else
+	i = m_priv->locs.insert(i, l);
+    }
+  return location(std::distance (m_priv->locs.begin(), i));
+}
+
+/// Given an instance of location type, return the triplet
+/// {path,line,column} that represents the source locus.  Note that
+/// the location must have been previously created from the function
+/// location_manager::expand_location otherwise this function yields
+/// unexpected results, including possibly a crash.
+///
+/// \param location the instance of location type to expand
+/// \param path the resulting path of the source locus
+/// \param line the resulting line of the source locus
+/// \param column the resulting colum of the source locus
+void
+location_manager::expand_location(const location	location,
+				  std::string&		path,
+				  unsigned&		line,
+				  unsigned&		column)
+{
+  expanded_location &l = m_priv->locs[location.m_value];
+  path = l.m_path;
+  line = l.m_line;
+  column = l.m_column;
+}
+
+}//end namespace abigail
--- src/abg-ir.h
+++ src/abg-ir.h
@@ -2,59 +2,122 @@
 #define __ABL_IR_H__
 
 #include <cstddef>
+#include <tr1/memory>
+#include <list>
 
-// A couple of forward declarations to avoid including too much cruft.
-namespace std
-{
-  class string;
-}
+using std::tr1::shared_ptr;
 
 // Our real stuff
 namespace abigail
 {
-  class location
-  {
-    location();
-    location (unsigned opaque);
+/// \brief The source location of a token.
+/// 
+/// This represents the location of a token coming from a given ABI
+/// Corpus.  This location is actually an abstraction of cursor in the
+/// table of all the locations of all the tokens of the ABI Corpus.
+/// That table is managed by the location_manager type.
+class location
+{
+  unsigned m_value;
 
-  public:
+  location();
+  location (unsigned);
 
-    location(const location&);
-    friend class location_manager;
-  };
+public:
 
-  class location_manager
-  {
-  public:
-    static location create_new_location(const std::string& file,
-					size_t line,
-					size_t column);
-    static void expand_location(const location&,
-				std::string& file,
-				size_t& line,
-				size_t& column);
-  };
+  friend class location_manager;
+};
 
-  /// \brief A declaration that introduces a scope.
-  class scope_decl
-  {
-  public:
-    scope_decl();
-  };
+/// \brief The entry point to manage locations.
+///
+/// This type keeps a table of all the locations for tokens of a
+/// given ABI Corpus
+class location_manager
+{
+  struct priv;
+  shared_ptr<priv> m_priv;
 
-  class decl
-  {
-  public:
-    decl(const std::string &name);
-    std::string&  get_name() const;
-  };
+public:
 
-  class basic_type
-  {
-  public:
-    basic_type();
-    
-  };
-}
+  location_manager();
+  location create_new_location(const std::string&	file,
+			       size_t			line,
+			       size_t			column);
+  void expand_location(const location	location,
+		       std::string&	path,
+		       unsigned&	line,
+		       unsigned&	column);
+};
 
+class scope_decl;
+
+/// \brief The base type of all declarations.
+class decl
+{
+  decl();
+public:
+
+  decl(const std::string& name,
+       const scope_decl& context,
+       location locus);
+  decl(const decl&);
+
+  location get_location() const;
+  void set_location(const location&);
+
+  scope_decl& get_context() const;
+};
+
+/// \brief A declaration that introduces a scope.
+class scope_decl : public decl
+{
+  scope_decl();
+
+public:
+
+  scope_decl(const std::string &name,
+	     const scope_decl& context,
+	     location& locus);
+  scope_decl(location&);
+
+  void add_member_decl(const shared_ptr<decl>);
+  const std::list<shared_ptr<decl> >& get_member_decls() const;
+};
+
+/// The an abstraction helper for type declarations
+class type_base
+{
+public:
+  type_base();
+  void set_size_in_bits(const size_t);
+  size_t get_size_in_bits() const;
+
+  void set_alignment_in_bits(const size_t);
+  size_t get_alignment_in_bits() const;
+};
+
+/// A basic type declaration that introduces no scope.
+class type_decl : public decl, public type_base
+{
+  type_decl();
+public:
+
+  type_decl(const std::string& name,
+	    const scope_decl& context,
+	    location locus);
+  type_decl(const type_decl&);
+};
+
+/// A type that introduces a scope.
+class scope_type_decl : public scope_decl, public type_base
+{
+  scope_type_decl();
+public:
+
+  scope_type_decl(const std::string& name,
+		  const scope_decl& context,
+		  location locus);
+};
+
+} // end namespace abigail
 #endif // __ABL_IR_H__
