7daf60f12c9f7b5620dafb011be220233348f46e
Dodji Seketeli <dodji@seketeli.org>
2013-07-16 21:58:43 +0200

    LGPLv3 License the library
    	* COPYING-LGPLV3:  New file.
    	* abg-config.cc: License the file to LGPLv3.
    	* abg-config.h: Likewise.
    	* abg-corpus.cc: Likewise.
    	* abg-corpus.h: Likewise.
    	* abg-hash.cc: Likewise.
    	* abg-hash.h: Likewise.
    	* abg-ir.cc: Likewise.
    	* abg-ir.h: Likewise.
    	* abg-libxml-utils.cc: Likewise.
    	* abg-libxml-utils.h: Likewise.
    	* abg-reader.cc: Likewise.
    	* abg-reader.h: Likewise.
    	* abg-writer.cc: Likewise.
    	* abg-writer.h: Likewise.
    	* src/abg-viz-common.cc: Likewise.
    	* src/abg-viz-common.h: Likewise.
    	* src/abg-viz-dot.cc: Likewise.
    	* src/abg-viz-dot.h: Likewise.
    	* src/abg-viz-svg.cc: Likewise.
    	* src/abg-viz-svg.h: Likewise.
    	* tests/test-read-write.cc: Likewise.
    	* tests/test-utils.cc: Likewise.
    	* tests/test-utils.h: Likewise.

--- src/abg-ir.h
+++ src/abg-ir.h
@@ -1,25 +1,22 @@
-// -*- mode: C++ -*-
+// -*- Mode: C++ -*-
+//
+// Copyright (C) 2013 Red Hat, Inc.
 //
 // This file is part of the GNU Application Binary Interface Generic
 // Analysis and Instrumentation Library (libabigail).  This library is
 // free software; you can redistribute it and/or modify it under the
-// terms of the GNU General Public License as published by the Free
-// Software Foundation; either version 3, or (at your option) any
+// terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option) any
 // later version.
 
 // This library is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.
+// General Lesser Public License for more details.
 
-// Under Section 7 of GPL version 3, you are granted additional
-// permissions described in the GCC Runtime Library Exception, version
-// 3.1, as published by the Free Software Foundation.
-
-// You should have received a copy of the GNU General Public License
-// and a copy of the GCC Runtime Library Exception along with this
-// program; see the files COPYING3 and COPYING.RUNTIME respectively.
-// If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program; see the file COPYING-LGPLV3.  If
+// not, see <http://www.gnu.org/licenses/>.
 
 /// @file
 
