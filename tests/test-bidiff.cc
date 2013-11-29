c5b81de47d63aa5515492f51c42b496e490aad26
Dodji Seketeli <dodji@redhat.com>
2013-11-29 13:49:50 +0100

    Add support for bidiff regression testing
    	* tests/data/test-bidiff/test-enum0-v0.cc.bi: New input file.
    	* tests/data/test-bidiff/test-enum0-v1.cc.bi: Likewise.
    	* tests/data/test-bidiff/test-enum0-report.diff: Likewise.
    	* tests/data/test-bidiff/test-enum1-v0.cc.bi: Likewise.
    	* tests/data/test-bidiff/test-enum1-v1.cc.bi: Likewise.
    	* tests/data/test-bidiff/test-enum1-report.txt: Likewise.
    	* tests/data/test-bidiff/test-qual-type0-v0.cc.bi: Likewise.
    	* tests/data/test-bidiff/test-qual-type0-v1.cc.bi: Likewise.
    	* tests/data/test-bidiff/test-qual-type0-report.txt: Likewise.
    	* tests/data/test-bidiff/test-struct0-v0.cc.bi: Likewise.
    	* tests/data/test-bidiff/test-struct0-v1.cc.bi: Likewise.
    	* tests/data/test-bidiff/test-struct0-report.txt: Likewise.
    	* tests/data/test-bidiff/test-struct1-v0.cc.bi: Likewise.
    	* tests/data/test-bidiff/test-struct1-v1.cc.bi: Likewise.
    	* tests/data/test-bidiff/test-struct1-report.txt: Likewise.
    	* tests/data/test-bidiff/test-var0-v0.cc.bi: Likewise.
    	* tests/data/test-bidiff/test-var0-v1.cc.bi: Likewise.
    	* tests/data/test-bidiff/test-var0-report.txt: Likewise.
    	* tests/test-bidiff.cc: New file.
    	* tests/Makefile.am: Build the new runtestbidiff regression test
    	and add the above to the source distribution.
    
    Signed-off-by: Dodji Seketeli <dodji@redhat.com>

---
+++ tests/test-bidiff.cc
@@ -0,0 +1,170 @@
+// -*- Mode: C++ -*-
+//
+// Copyright (C) 2013 Red Hat, Inc.
+//
+// This file is part of the GNU Application Binary Interface Generic
+// Analysis and Instrumentation Library (libabigail).  This library is
+// free software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option) any
+// later version.
+
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Lesser Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program; see the file COPYING-LGPLV3.  If
+// not, see <http://www.gnu.org/licenses/>.
+
+// Author: Dodji Seketeli
+
+/// @file
+///
+/// This program runs a diff between input files and compares the
+/// resulting report with a reference report.  If the resulting report
+/// is different from the reference report, the test has failed.
+///
+/// The set of input files and reference reports to consider should be
+/// present in the source distribution.
+
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+#include "abg-tools-utils.h"
+#include "test-utils.h"
+#include "abg-comparison.h"
+
+using std::string;
+using std::ofstream;
+using std::cerr;
+
+struct InOutSpec
+{
+  const char* first_in_path;
+  const char* second_in_path;
+  const char* ref_diff_path;
+  const char* out_path;
+};// end struct InOutSpec
+
+static InOutSpec specs[] =
+{
+  {
+    "data/test-bidiff/test-enum0-v0.cc.bi",
+    "data/test-bidiff/test-enum0-v1.cc.bi",
+    "data/test-bidiff/test-enum0-report.txt",
+    "output/test-bidiff/test-enum0-report.txt"
+  },
+  {
+    "data/test-bidiff/test-enum1-v0.cc.bi",
+    "data/test-bidiff/test-enum1-v1.cc.bi",
+    "data/test-bidiff/test-enum1-report.txt",
+    "output/test-bidiff/test-enum1-report.txt"
+  },
+  {
+    "data/test-bidiff/test-qual-type0-v0.cc.bi",
+    "data/test-bidiff/test-qual-type0-v1.cc.bi",
+    "data/test-bidiff/test-qual-type0-report.txt",
+    "output/test-bidiff/test-qual-type0-report.txt"
+  },
+  {
+    "data/test-bidiff/test-struct0-v0.cc.bi",
+    "data/test-bidiff/test-struct0-v1.cc.bi",
+    "data/test-bidiff/test-struct0-report.txt",
+    "output/test-bidiff/test-struct0-report.txt"
+  },
+  {
+    "data/test-bidiff/test-struct1-v0.cc.bi",
+    "data/test-bidiff/test-struct1-v1.cc.bi",
+    "data/test-bidiff/test-struct1-report.txt",
+    "output/test-bidiff/test-struct1-report.txt"
+  },
+  {
+    "data/test-bidiff/test-var0-v0.cc.bi",
+    "data/test-bidiff/test-var0-v1.cc.bi",
+    "data/test-bidiff/test-var0-report.txt",
+    "output/test-bidiff/test-var0-report.txt"
+  },
+  // This should be the last entry.
+  {0, 0, 0, 0}
+};
+
+#define  NUM_SPEC_ELEMS \
+  ((sizeof(specs) / sizeof(InOutSpec)) - 1)
+
+using std::string;
+using std::cerr;
+using std::ofstream;
+using abigail::translation_unit;
+using abigail::translation_unit_sptr;
+using abigail::comparison::translation_unit_diff_sptr;
+using abigail::comparison::compute_diff;
+
+int
+main(int, char*[])
+{
+  bool is_ok = true;
+
+  string out_path =
+    abigail::tests::get_build_dir() + "/tests/" + specs->out_path;
+  if (!abigail::tools::ensure_parent_dir_created(out_path))
+    {
+      cerr << "Could not create parent director for " << out_path;
+      return 1;
+    }
+
+  string first_in_path, second_in_path, ref_diff_path;
+  for (InOutSpec *s = specs; s->first_in_path; ++s)
+    {
+      first_in_path =
+	abigail::tests::get_src_dir() + "/tests/" + s->first_in_path;
+      second_in_path =
+	abigail::tests::get_src_dir() + "/tests/" + s->second_in_path;
+      ref_diff_path =
+	abigail::tests::get_src_dir() + "/tests/" + s->ref_diff_path;
+      out_path =
+	abigail::tests::get_build_dir() + "/tests/" + s->out_path;
+
+      if (!abigail::tools::ensure_parent_dir_created(out_path))
+	{
+	  cerr << "Could not create parent director for " << out_path;
+	  continue;
+	}
+
+      translation_unit_sptr tu1(new translation_unit(first_in_path));
+      if (!tu1->read())
+	{
+	  cerr << "failed to read " << tu1->get_path() << "\n";
+	  is_ok = false;
+	  continue;
+	}
+
+      translation_unit_sptr tu2(new translation_unit(second_in_path));
+      if (!tu2->read())
+	{
+	  cerr << "failed to read " << tu1->get_path() << "\n";
+	  is_ok = false;
+	  continue;
+	}
+
+      translation_unit_diff_sptr d = compute_diff(tu1, tu2);
+      ofstream of(s->out_path, std::ios_base::trunc);
+      if (!of.is_open())
+	{
+	  cerr << "failed to read " << s->out_path << "\n";
+	  is_ok = false;
+	  continue;
+	}
+
+      d->report(of);
+      of.close();
+
+      string cmd = "diff -u " + ref_diff_path + " " + out_path;
+      if (system(cmd.c_str()))
+	is_ok = false;
+    }
+
+  return !is_ok;
+}
