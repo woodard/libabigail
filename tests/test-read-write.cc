a11912d96420b1bac84a778d5da64d93c1a08d28
Dodji Seketeli <dodji@redhat.com>
2014-07-01 09:42:25 +0200

    Add comment to test-read-write.cc
    	* tests/test-read-write.cc: Update copyright notice and add a
    	meaningful comment for the file.
    
    Signed-off-by: Dodji Seketeli <dodji@redhat.com>

--- tests/test-read-write.cc
+++ tests/test-read-write.cc
@@ -1,6 +1,6 @@
 // -*- Mode: C++ -*-
 //
-// Copyright (C) 2013 Red Hat, Inc.
+// Copyright (C) 2013-2014 Red Hat, Inc.
 //
 // This file is part of the GNU Application Binary Interface Generic
 // Analysis and Instrumentation Library (libabigail).  This library is
@@ -17,6 +17,10 @@
 // You should have received a copy of the GNU Lesser General Public
 // License along with this program; see the file COPYING-LGPLV3.  If
 // not, see <http://www.gnu.org/licenses/>.
+
+/// @file read an XML corpus file (in the native Abigail XML format),
+/// save it back and diff the resulting XML file against the input
+/// file.  They should be identical.
 
 #include <string>
 #include <fstream>
