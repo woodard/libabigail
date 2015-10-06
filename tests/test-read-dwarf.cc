fcea5dffcecacd428cc9b4cffbf02570aff2db7d
Ondrej Oprala <ooprala@redhat.com>
2015-10-06 14:35:14 +0200

    Parallelize test read-dwarf.
    	* tests/Makefile.am: Link runtestreaddwarf with libpthread.
    	* tests/test-read-dwarf.cc (main) Create worker threads corresponding
    	to the number of CPUs online, add a "--no-parallel" option and move
    	the main loop...
    	(handleInOutSpec) ...here.
    
    Signed-off-by: Ondrej Oprala <ooprala@redhat.com>

--- tests/test-read-dwarf.cc
+++ tests/test-read-dwarf.cc
@@ -28,6 +28,8 @@
 #include <fstream>
 #include <iostream>
 #include <cstdlib>
+#include <unistd.h>
+#include <pthread.h>
 #include "abg-ir.h"
 #include "abg-dwarf-reader.h"
 #include "abg-writer.h"
@@ -38,6 +40,7 @@ using std::string;
 using std::ofstream;
 using std::cerr;
 using abigail::tests::get_build_dir;
+using abigail::dwarf_reader::read_corpus_from_elf;
 
 /// This is an aggregate that specifies where a test shall get its
 /// input from, and where it shall write its ouput to.
@@ -155,96 +158,143 @@ InOutSpec in_out_specs[] =
   {NULL, NULL, NULL}
 };
 
-int
-main()
-{
-  using abigail::dwarf_reader::read_corpus_from_elf;
-  unsigned result = 1;
+// The global pointer to the testsuite paths.
+InOutSpec *iospec = in_out_specs;
+// Lock to help atomically increment iospec.
+pthread_mutex_t spec_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t write_lock = PTHREAD_MUTEX_INITIALIZER;
+// No lock needed here, since is_ok is only ever re-set to false.
+bool is_ok = true;
 
-  bool is_ok = true;
+// These prefixes don't change during the program's lifetime, so
+// we only get them once.
+const string out_abi_base = get_build_dir() + "/tests/";
+const string in_elf_base  = abigail::tests::get_src_dir() + "/tests/";
+const string in_abi_base = in_elf_base;
+
+void
+handle_in_out_spec(void)
+{
   string in_elf_path, in_abi_path, out_abi_path;
   abigail::ir::environment_sptr env;
-  abigail::corpus_sptr corp;
+  InOutSpec *s;
 
-  for (InOutSpec* s = in_out_specs; s->in_elf_path; ++s)
+  while (true)
+  {
+    pthread_mutex_lock(&spec_lock);
+    if (iospec->in_elf_path)
+      s = iospec++;
+    else
+      s = NULL;
+    pthread_mutex_unlock(&spec_lock);
+
+    if (!s)
+      pthread_exit(NULL);
+    in_elf_path = in_elf_base + s->in_elf_path;
+    env.reset(new abigail::ir::environment);
+    abigail::dwarf_reader::status status =
+      abigail::dwarf_reader::STATUS_UNKNOWN;
+    abigail::corpus_sptr corp =
+      read_corpus_from_elf(in_elf_path,
+			   /*debug_info_root_path=*/0,
+			   env.get(),
+			   /*load_all_types=*/false,
+			   status);
+    if (!corp)
+      {
+	cerr << "failed to read " << in_elf_path << "\n";
+	is_ok = false;
+	continue;
+      }
+    corp->set_path(s->in_elf_path);
+    // Do not take architecture names in comparison so that these
+    // test input binaries can come from whatever arch the
+    // programmer likes.
+    corp->set_architecture_name("");
+
+    out_abi_path = out_abi_base + s->out_abi_path;
+    if (!abigail::tools_utils::ensure_parent_dir_created(out_abi_path))
+      {
+	cerr << "Could not create parent directory for " << out_abi_path;
+	is_ok = false;
+	exit(1);
+      }
+
+    ofstream of(out_abi_path.c_str(), std::ios_base::trunc);
+    if (!of.is_open())
+      {
+	cerr << "failed to read " << out_abi_path << "\n";
+	is_ok = false;
+	continue;
+      }
+
+    pthread_mutex_lock(&write_lock);
+    is_ok =
+      abigail::xml_writer::write_corpus_to_native_xml(corp,
+						      /*indent=*/0,
+						      of);
+    pthread_mutex_unlock(&write_lock);
+    of.close();
+
+    string abilint = get_build_dir() + "/tools/abilint";
+    abilint += " --noout";
+    string cmd = abilint + " " + out_abi_path;
+    if (system(cmd.c_str()))
+      {
+	cerr << "output file doesn't pass abilint: " << out_abi_path << "\n";
+	is_ok = false;
+      }
+
+    string abidiff = get_build_dir() + "/tools/abidiff";
+    cmd = abidiff + " --no-architecture " + in_elf_path + " " + out_abi_path;
+    if (system(cmd.c_str()))
+      {
+	cerr << "ABIs differ:\n"
+	     << in_elf_path
+	     << "\nand:\n"
+	     << out_abi_path
+	     << "\n";
+	is_ok = false;
+      }
+
+    in_abi_path = in_abi_base +  s->in_abi_path;
+    cmd = "diff -u " + in_abi_path + " " + out_abi_path;
+    if (system(cmd.c_str()))
+      is_ok = false;
+  }
+}
+
+int
+main(int argc, char *argv[])
+{
+  // Number of currently online processors in the system.
+  size_t nprocs = sysconf(_SC_NPROCESSORS_ONLN);
+  // All the pthread_ts we've created.
+  pthread_t *pthr = new pthread_t[nprocs];
+
+  if (argc == 2)
     {
-      in_elf_path = abigail::tests::get_src_dir() + "/tests/" + s->in_elf_path;
-      env.reset(new abigail::ir::environment);
-      abigail::dwarf_reader::status status =
-	abigail::dwarf_reader::STATUS_UNKNOWN;
-      corp = read_corpus_from_elf(in_elf_path,
-				  /*debug_info_root_path=*/0,
-				  env.get(),
-				  /*load_all_types=*/false,
-				  status);
-      if (!corp)
+      if (argv[1] == string("--no-parallel"))
+	nprocs = 1;
+      else
 	{
-	  cerr << "failed to read " << in_elf_path << "\n";
-	  is_ok = false;
-	  continue;
+	  cerr << "unrecognized option\n";
+	  cerr << "usage: " << argv[0] << " [--no-parallel]\n" ;
+	  return 1;
 	}
-      corp->set_path(s->in_elf_path);
-      // Do not take architecture names in comparison so that these
-      // test input binaries can come from whatever arch the
-      // programmer likes.
-      corp->set_architecture_name("");
-
-      out_abi_path = get_build_dir() + "/tests/" + s->out_abi_path;
-      if (!abigail::tools_utils::ensure_parent_dir_created(out_abi_path))
-	{
-	  cerr << "Could not create parent director for " << out_abi_path;
-	  is_ok = false;
-	  return result;
-	}
-
-      ofstream of(out_abi_path.c_str(), std::ios_base::trunc);
-      if (!of.is_open())
-	{
-	  cerr << "failed to read " << out_abi_path << "\n";
-	  is_ok = false;
-	  continue;
-	}
-
-      bool r =
-	abigail::xml_writer::write_corpus_to_native_xml(corp,
-							/*indent=*/0,
-							of);
-      is_ok = (is_ok && r);
-      of.close();
-
-      string abilint = get_build_dir() + "/tools/abilint";
-      abilint += " --noout";
-      string cmd = abilint + " " + out_abi_path;
-      if (system(cmd.c_str()))
-	{
-	  cerr << "output file doesn't pass abilint: " << out_abi_path << "\n";
-	  r = false;
-	}
-      is_ok = (is_ok && r);
-
-
-      in_elf_path = abigail::tests::get_src_dir() + "/tests/" + s->in_elf_path;
-      string abidiff = get_build_dir() + "/tools/abidiff";
-      cmd = abidiff + " --no-architecture " + in_elf_path + " " + out_abi_path;
-      r = true;
-      if (system(cmd.c_str()))
-	{
-	  cerr << "ABIs differ:\n"
-	       << in_elf_path
-	       << "\nand:\n"
-	       << out_abi_path
-	       << "\n";
-	  r = false;
-	}
-      is_ok = (is_ok && r);
-
-      in_abi_path = abigail::tests::get_src_dir() + "/tests/" + s->in_abi_path;
-      cmd = "diff -u " + in_abi_path + " " + out_abi_path;
-      r = true;
-      if (system(cmd.c_str()))
-	r = false;
-      is_ok = (is_ok && r);
     }
+
+  assert(nprocs >= 1);
+
+  for (size_t i = 0; i < nprocs; ++i)
+    pthread_create(&pthr[i], NULL,
+		   (void*(*)(void*))handle_in_out_spec,
+		   NULL);
+
+  for (size_t i = 0; i < nprocs; ++i)
+    pthread_join(pthr[i], NULL);
+
+  delete pthr;
 
   return !is_ok;
 }
