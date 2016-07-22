a126e925837bdc5ef6340150aa00ba2df6ec44e2
Dodji Seketeli <dodji@redhat.com>
2016-07-22 11:01:41 +0200

    Prepare support for symbol visibility control
    While working on something else, I started to prepare the support for
    visibility control in the library.  This patch detects that we are
    using a compiler that supports GCC's visibility control attributes and
    defines macros ABG_HIDDEN, ABG_EXPORTED,
    ABG_{BEGIN,END}_EXPORT_DECLARATIONS to control the visibility of
    declarations accordingly.  These are not yet used but using them
    should now be easy.
    
    	* configure.ac: Detect compiler support for
    	__attribute__((visibility("hidden")) and define the
    	HAS_GCC_VISIBILITY_ATTRIBUTE macro accordingly.  Update the
    	configuration report.
    	* src/abg-internal.h: New internal header file that defines macros
    	to be used in the source code to control declaration visibility.
    	* src/Makefile.am: Add abg-internal.h to source distribution.  Add
    	src/ to the include search path.
    
    Signed-off-by: Dodji Seketeli <dodji@redhat.com>

---
+++ src/abg-internal.h
@@ -0,0 +1,50 @@
+// -*- Mode: C++ -*-
+//
+// Copyright (C) 2016 Red Hat, Inc.
+//
+// This file is part of the GNU Application Binary Interface Generic
+// Analysis and Instrumentation Library (libabigail).  This library is
+// free software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option) any
+// later version.
+
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Lesser Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program; see the file COPYING-LGPLV3.  If
+// not, see <http://www.gnu.org/licenses/>.
+//
+// Author: Dodji Seketeli
+
+#ifndef __ABG_INTERNAL_H__
+#define __ABG_INTERNAL_H__
+#include "config.h"
+
+#ifdef HAS_GCC_VISIBILITY_ATTRIBUTE
+
+/// This macro makes a declaration be hidden at the binary level.
+///
+/// On ELF systems, this means that the symbol for the declaration
+/// (function or variable) is going to be local to the file.  External
+/// ELF files won't be able to link against the symbol.
+#define ABG_HIDDEN  __attribute__((visibility("hidden")))
+
+/// This macro makes a declaration be exported at the binary level.
+///
+/// On ELF systems, this means that the symbol for the declaration
+///(function or variable) is going to be global.  External ELF files
+///will be able to link against the symbol.
+#define ABG_EXPORTED __attribute__((visibility("default")))
+#define ABG_BEGIN_EXPORT_DECLARATIONS #pagma GCC visibility push(default)
+#define ABG_END_EXPORT_DECLARATIONS #pragma GCC visibility pop
+#else
+#define ABG_HIDDEN
+#define ABG_EXPORTED
+#define ABG_BEGIN_EXPORT_DECLARATIONS
+#define ABG_END_EXPORT_DECLARATIONS
+#endif
+#endif // __ABG_INTERNAL_H__
