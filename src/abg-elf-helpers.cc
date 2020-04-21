b4d97cdd0ff766c197ca855eb550429308e2f098
Matthias Maennich <maennich@google.com>
2020-04-21 08:35:44 +0200

    abg-dwarf-reader split: create abg-elf-helpers.{h,cc} and test case
    abg-elf-helpers.{h,cc} shall contain the ELF related parts of the
    abg-dwarf-reader. Create the stub files, an empty unit test and hook
    everything up in the make system.
    
    	* src/Makefile.am: Add new source files abg-elf-helpers.{h,cc}.
    	* src/abg-elf-helpers.cc: New source file.
    	* src/abg-elf-helpers.h: New header file.
    	* tests/.gitignore: Exclude runtestelfhelpers from being committed.
    	* tests/Makefile.am: Add new test case runtestelfhelpers.
    	* tests/test-elf-helpers.cc: New test source file.
    
    Reviewed-by: Giuliano Procida <gprocida@google.com>
    Signed-off-by: Matthias Maennich <maennich@google.com>
    Signed-off-by: Dodji Seketeli <dodji@redhat.com>

---
+++ src/abg-elf-helpers.cc
@@ -0,0 +1,34 @@
+// -*- Mode: C++ -*-
+//
+// Copyright (C) 2020 Google, Inc.
+//
+// This file is part of the GNU Application Binary Interface Generic
+// Analysis and Instrumentation Library (libabigail).  This library is
+// free software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option) any
+// later version.
+
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Lesser Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program; see the file COPYING-LGPLV3.  If
+// not, see <http://www.gnu.org/licenses/>.
+
+/// @file
+///
+/// This contains the definitions of the ELF utilities for the dwarf reader.
+
+#include "abg-elf-helpers.h"
+
+namespace abigail
+{
+
+namespace elf_helpers
+{
+
+} // end namespace elf_helpers
+} // end namespace abigail
---
+++ src/abg-elf-helpers.h
@@ -0,0 +1,37 @@
+// -*- Mode: C++ -*-
+//
+// Copyright (C) 2020 Google, Inc.
+//
+// This file is part of the GNU Application Binary Interface Generic
+// Analysis and Instrumentation Library (libabigail).  This library is
+// free software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option) any
+// later version.
+
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Lesser Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program; see the file COPYING-LGPLV3.  If
+// not, see <http://www.gnu.org/licenses/>.
+
+/// @file
+///
+/// This contains a set of ELF utilities used by the dwarf reader.
+
+#ifndef __ABG_ELF_HELPERS_H__
+#define __ABG_ELF_HELPERS_H__
+
+namespace abigail
+{
+
+namespace elf_helpers
+{
+
+} // end namespace elf_helpers
+} // end namespace abigail
+
+#endif // __ABG_ELF_HELPERS_H__
