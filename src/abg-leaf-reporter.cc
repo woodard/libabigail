4e256018d3b4461009faba99f1c36b9517db78f6
Giuliano Procida <gprocida@google.com>
2020-06-16 13:12:10 +0100

    Fix leaf-mode formatting of decl <-> defn diffs.
    This commit adds a missing newline.
    
    	* src/abg-leaf-reporter.cc (leaf_reporter::report): In the
    	class_or_union_diff overload, add a terminating new line after
    	reporting a declaration-only <-> definition difference.
    
    Signed-off-by: Giuliano Procida <gprocida@google.com>

--- src/abg-leaf-reporter.cc
+++ src/abg-leaf-reporter.cc
@@ -475,7 +475,7 @@ leaf_reporter::report(const class_or_union_diff& d,
 	  : " and is now a defined type";
 
 	out << indent << "type " << first->get_pretty_representation()
-	    << was << is_now;
+	    << was << is_now << "\n";
 	return;
       }
 
