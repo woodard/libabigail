07816b2d59f36be287e397be4f766b588819f2ac
Matthias Maennich <maennich@google.com>
2021-01-27 12:58:37 +0000

    dwarf-reader split: create abg-symtab-reader.{h,cc} and test case
    abg-symtab-reader.{h,cc} shall contain the refactored symtab reader.
    Create the stub files, an empty unit test and hook everything up in the
    make system.
    
        * src/abg-symtab-reader.h: New header file.
        * src/abg-symtab-reader.cc: New source file.
        * src/Makefile.am: Add new source files.
        * tests/Makefile.am: Add new test case runtestsymtabreader.
        * tests/test-symtab-reader.cc: New test source file.
    
    Reviewed-by: Giuliano Procida <gprocida@google.com>
    Signed-off-by: Matthias Maennich <maennich@google.com>

---
+++ src/abg-symtab-reader.cc
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// -*- Mode: C++ -*-
+//
+// Copyright (C) 2020 Google, Inc.
+//
+// Author: Matthias Maennich
+
+/// @file
+///
+/// This contains the definition of the symtab reader
+
+#include "abg-symtab-reader.h"
+
+namespace abigail
+{
+
+namespace symtab_reader
+{
+
+} // end namespace symtab_reader
+} // end namespace abigail
