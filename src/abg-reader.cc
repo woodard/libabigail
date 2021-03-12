9c0e580e5894c995c016875c0ac23c0f4159df70
Ben Woodard <woodard@redhat.com>
2021-03-12 11:22:55 -0800

    Update the copyright to new year.
    Update the copyright on files touched removing the zip archive support
    
           * include/abg-reader.h Update copyright
           * include/abg-tools-utils.h Update copyright
           * src/abg-corpus.cc Update copyright
           * src/abg-reader.cc Update copyright
           * src/abg-tools-utils.cc Update copyright
           * src/abg-writer.cc Update copyright
           * tools/abidiff.cc Update copyright
           * tools/abilint.cc Update copyright

--- src/abg-reader.cc
+++ src/abg-reader.cc
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 // -*- mode: C++ -*-
 //
-// Copyright (C) 2013-2020 Red Hat, Inc.
+// Copyright (C) 2013-2021 Red Hat, Inc.
 
 /// @file
 ///
