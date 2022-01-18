woodard/libabigail

commit 16207c4af7bc2510ba5d996b8dbdecda30d15200

Bug 28191 - Interpret DWARF 5 addrx locations
This change uses libdw facilities to interpret location expressions
instead of using libabigail's own mini-interpreter. With the fix for
elfutils https://sourceware.org/bugzilla/show_bug.cgi?id=28220 in
elfutils-0.186, abidw will correctly interpret Clang DWARF 5 symbol
addresses. Without that fix many declarations will not be linked to
their corresponding symbols due to the incorrect interpretation of
location attribute data.

	* src/abg-dwarf-reader.cc (die_location_address): Use
	dwarf_attr_integrate, dwarf_getlocation and
	dwarf_getlocation_attr to decode addreses, instead of
	die_location_expr and eval_last_constant_dwarf_sub_expr.
	* tests/data/test-read-dwarf/PR25042-libgdbm-clang-dwarf5.so.6.0.0.abi:
	Refresh test reference output; two more symbols have types.

Signed-off-by: Giuliano Procida <gprocida@google.com>

src/abg-dwarf-reader.cc
@@ -8855,17 +8855,30 @@ die_location_address(Dwarf_Die*	die,
   uint64_t expr_len = 0;
 
   is_tls_address = false;
-  if (!die_location_expr(die, DW_AT_location, &expr, &expr_len))
+
+  if (!die)
     return false;
 
-  int64_t addr = 0;
-  if (!eval_last_constant_dwarf_sub_expr(expr, expr_len, addr, is_tls_address))
+  Dwarf_Attribute attr;
+  if (!dwarf_attr_integrate(const_cast<Dwarf_Die*>(die), DW_AT_location, &attr))
     return false;
 
-  address = addr;
+  if (dwarf_getlocation(&attr, &expr, &expr_len))
+    return false;
+  // Ignore location expressions where reading them succeeded but
+  // their length is 0.
+  if (expr_len == 0)
+    return false;
+
+  Dwarf_Attribute result;
+  if (!dwarf_getlocation_attr(&attr, expr, &result))
+    // A location that has been interpreted as an address.
+    return !dwarf_formaddr(&result, &address);
+
+  // Just get the address out of the number field.
+  address = expr->number;
   return true;
 }
-
 
 /// Return the index of a function in its virtual table.  That is,
 /// return the value of the DW_AT_vtable_elem_location attribute.

commit 6e0d3f7278465bcfc2ee09b9cfb06f81bb761e4f

DWARF reader: use size_t for DWARF expression length
A recent change broke 32-bit builds due to an implicit assumption that
size_t == uint64_t. This appears in the elfutils dwarf_getlocation*
functions' types.

This commit updates callers and other functions to use size_t
consistently for such expression lengths and indexes.

	* src/abg-dwarf-reader.cc (die_location_expr): Change expr_len
	argument type to size_t*.
	(op_manipulates_stack): Change expr_len and index argument
	types to size_t; change next_index argument type to size_t&.
	(eval_last_constant_dwarf_sub_expr): Change expr_len argument
	and local variables index and next_index types to size_t.
	(die_member_offset): Change local variable expr_len type to
	size_t.
	(die_location_address): Likewise.
	(die_virtual_function_index): Likewise.

Fixes: 16207c4af7bc ("Bug 28191 - Interpret DWARF 5 addrx locations")
Signed-off-by: Giuliano Procida <gprocida@google.com>
Signed-off-by: Dodji Seketeli <dodji@redhat.com>

src/abg-dwarf-reader.cc
@@ -7686,7 +7686,7 @@ static bool
 die_location_expr(const Dwarf_Die* die,
 		  unsigned attr_name,
 		  Dwarf_Op** expr,
-		  uint64_t* expr_len)
+		  size_t* expr_len)
 {
   if (!die)
     return false;
@@ -8022,9 +8022,9 @@ op_pushes_non_constant_value(Dwarf_Op* ops,
 /// DEVM stack, false otherwise.
 static bool
 op_manipulates_stack(Dwarf_Op* expr,
-		     uint64_t expr_len,
-		     uint64_t index,
-		     uint64_t& next_index,
+		     size_t expr_len,
+		     size_t index,
+		     size_t& next_index,
 		     dwarf_expr_eval_context& ctxt)
 {
   Dwarf_Op& op = expr[index];
@@ -8398,7 +8398,7 @@ eval_quickly(Dwarf_Op*	expr,
 /// to evaluate, false otherwise.
 static bool
 eval_last_constant_dwarf_sub_expr(Dwarf_Op*	expr,
-				  uint64_t	expr_len,
+				  size_t	expr_len,
 				  int64_t&	value,
 				  bool&	is_tls_address,
 				  dwarf_expr_eval_context &eval_ctxt)
@@ -8407,7 +8407,7 @@ eval_last_constant_dwarf_sub_expr(Dwarf_Op*	expr,
   // expression contained in the DWARF expression 'expr'.
   eval_ctxt.reset();
 
-  uint64_t index = 0, next_index = 0;
+  size_t index = 0, next_index = 0;
   do
     {
       if (op_is_arith_logic(expr, expr_len, index,
@@ -8452,7 +8452,7 @@ eval_last_constant_dwarf_sub_expr(Dwarf_Op*	expr,
 /// to evaluate, false otherwise.
 static bool
 eval_last_constant_dwarf_sub_expr(Dwarf_Op*	expr,
-				  uint64_t	expr_len,
+				  size_t	expr_len,
 				  int64_t&	value,
 				  bool&	is_tls_address)
 {
@@ -8775,7 +8775,7 @@ die_member_offset(const read_context& ctxt,
 		  int64_t& offset)
 {
   Dwarf_Op* expr = NULL;
-  uint64_t expr_len = 0;
+  size_t expr_len = 0;
   uint64_t bit_offset = 0;
 
   // First let's see if the DW_AT_data_bit_offset attribute is
@@ -8852,7 +8852,7 @@ die_location_address(Dwarf_Die*	die,
 		     bool&		is_tls_address)
 {
   Dwarf_Op* expr = NULL;
-  uint64_t expr_len = 0;
+  size_t expr_len = 0;
 
   is_tls_address = false;
 
@@ -8898,7 +8898,7 @@ die_virtual_function_index(Dwarf_Die* die,
     return false;
 
   Dwarf_Op* expr = NULL;
-  uint64_t expr_len = 0;
+  size_t expr_len = 0;
   if (!die_location_expr(die, DW_AT_vtable_elem_location,
 			 &expr, &expr_len))
     return false;