woodard/libabigail

commit 7bd69830520f2e7ce4efb989b5fd23fde316c7c1

Make Front Ends first class citizens
This patch is a reorganization of the code to better support the need
for having several different front-ends.

In the libabigail pipeline of operation, a front-end is the part of
the pipeline that analyses the input file.  For instance, to analyse
an ELF file, there is going to be one front-end.  To analyse an ABIXML
file, there is going to be another front-end.

The middle-end is the part of the pipeline that interacts with the
internal representation of ABIs.  The middle-end knows how to analyse,
compare ABI corpora provide an internal representation of the
comparison result and analyse it as well.

The back-end would be the part of the front-end that knows how to
serialize internal representations of ABIs and ABI comparison results.

One could thus imagine a front-end that understands the DWARF debug
info format embedded in an ELF file.  Another front-end would be
dedicated to the CTF debug info format, and so on.

Front-ends can share capabilities.  For instance, DWARF and CTF
front-ends are ELF based front end.  As such, they share capabilities
to understand the ELF format.  They don't share much with the ABIXML
front-end, however, as it's based on XML, which has almost nothing in
common with ELF.

To support this organization of concepts, this patch introduces a new
hierarchy of types in the form of C++ classes.

All front-ends implements the "Front End Interface".  As such, they
all inherit the abigail::fe_iface class.

That class provides properties and behaviours that are shared by all
front-ends that libabigail supports today.  Namely, that class
provides access to some of the options that are relevant to operating
the front-end, to the ABI corpus or corpus group being constructed and
to the suppression specifications that are considered.  It also
provides an abstract interface to perform the actual loading of the
ABI corpus.  That abstract interface has to be implemented by every

src/abg-elf-helpers.cc
@@ -6,11 +6,13 @@
 /// @file
 ///
 /// This contains the definitions of the ELF utilities for the dwarf reader.
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <limits.h>
+#include <elfutils/libdwfl.h>
 #include "abg-elf-helpers.h"
-
-#include <elf.h>
-
 #include "abg-tools-utils.h"
 
 namespace abigail
@@ -1380,6 +1382,179 @@ address_is_in_opd_section(Elf* elf_handle, Dwarf_Addr addr)
   return false;
 }
 
+/// Get data tag information of an ELF file by looking up into its
+/// dynamic segment
+///
+/// @param elf the elf handle to use for the query.
+///
+/// @param dt_tag data tag to look for in dynamic segment
+/// @param dt_tag_data vector of found information for a given @p data_tag
+///
+/// @return true iff data tag @p data_tag was found
+bool
+lookup_data_tag_from_dynamic_segment(Elf*                       elf,
+                                     Elf64_Sxword               data_tag,
+                                     vector<string>&            dt_tag_data)
+{
+  size_t num_prog_headers = 0;
+  bool found = false;
+  if (elf_getphdrnum(elf, &num_prog_headers) < 0)
+    return found;
+
+  // Cycle through each program header.
+  for (size_t i = 0; i < num_prog_headers; ++i)
+    {
+      GElf_Phdr phdr_mem;
+      GElf_Phdr *phdr = gelf_getphdr(elf, i, &phdr_mem);
+      if (phdr == NULL || phdr->p_type != PT_DYNAMIC)
+        continue;
+
+      // Poke at the dynamic segment like a section, so that we can
+      // get its section header information; also we'd like to read
+      // the data of the segment by using elf_getdata() but that
+      // function needs a Elf_Scn data structure to act on.
+      // Elfutils doesn't really have any particular function to
+      // access segment data, other than the functions used to
+      // access section data.
+      Elf_Scn *dynamic_section = gelf_offscn(elf, phdr->p_offset);
+      GElf_Shdr  shdr_mem;
+      GElf_Shdr *dynamic_section_header = gelf_getshdr(dynamic_section,
+						       &shdr_mem);
+      if (dynamic_section_header == NULL
+          || dynamic_section_header->sh_type != SHT_DYNAMIC)
+        continue;
+
+      // Get data of the dynamic segment (seen as a section).
+      Elf_Data *data = elf_getdata(dynamic_section, NULL);
+      if (data == NULL)
+        continue;
+
+      // Get the index of the section headers string table.
+      size_t string_table_index = 0;
+      ABG_ASSERT (elf_getshdrstrndx(elf, &string_table_index) >= 0);
+
+      size_t dynamic_section_header_entry_size = gelf_fsize(elf,
+                                                            ELF_T_DYN, 1,
+                                                            EV_CURRENT);
+
+      GElf_Shdr link_mem;
+      GElf_Shdr *link =
+        gelf_getshdr(elf_getscn(elf,
+                                dynamic_section_header->sh_link),
+		     &link_mem);
+      ABG_ASSERT(link != NULL);
+
+      size_t num_dynamic_section_entries =
+        dynamic_section_header->sh_size / dynamic_section_header_entry_size;
+
+      // Now walk through all the DT_* data tags that are in the
+      // segment/section
+      for (size_t j = 0; j < num_dynamic_section_entries; ++j)
+        {
+          GElf_Dyn dynamic_section_mem;
+          GElf_Dyn *dynamic_section = gelf_getdyn(data,
+                                                  j,
+                                                  &dynamic_section_mem);
+          if (dynamic_section->d_tag == data_tag)
+            {
+              dt_tag_data.push_back(elf_strptr(elf,
+                                               dynamic_section_header->sh_link,
+					       dynamic_section->d_un.d_val));
+              found = true;
+            }
+        }
+    }
+  return found;
+}
+
+const Dwfl_Callbacks&
+initialize_dwfl_callbacks(Dwfl_Callbacks& cb,
+			  char** debug_info_root_path)
+{
+  cb.find_debuginfo = dwfl_standard_find_debuginfo;
+  cb.section_address = dwfl_offline_section_address;
+  cb.debuginfo_path = debug_info_root_path;
+  return cb;
+}
+
+dwfl_sptr
+create_new_dwfl_handle(Dwfl_Callbacks& cb)
+{
+  dwfl_sptr handle(dwfl_begin(&cb), dwfl_deleter());
+  return handle;
+}
+
+/// Fetch the SONAME ELF property from an ELF binary file.
+///
+/// @param path The path to the elf file to consider.
+///
+/// @param soname out parameter. Set to the SONAME property of the
+/// binary file, if it present in the ELF file.
+///
+/// return false if an error occured while looking for the SONAME
+/// property in the binary, true otherwise.
+bool
+get_soname_of_elf_file(const string& path, string &soname)
+{
+
+  int fd = open(path.c_str(), O_RDONLY);
+  if (fd == -1)
+    return false;
+
+  elf_version (EV_CURRENT);
+  Elf* elf = elf_begin (fd, ELF_C_READ_MMAP, NULL);
+
+  GElf_Ehdr ehdr_mem;
+  GElf_Ehdr* ehdr = gelf_getehdr (elf, &ehdr_mem);
+  if (ehdr == NULL)
+    return false;
+
+  for (int i = 0; i < ehdr->e_phnum; ++i)
+    {
+      GElf_Phdr phdr_mem;
+      GElf_Phdr* phdr = gelf_getphdr (elf, i, &phdr_mem);
+
+      if (phdr != NULL && phdr->p_type == PT_DYNAMIC)
+        {
+          Elf_Scn* scn = gelf_offscn (elf, phdr->p_offset);
+          GElf_Shdr shdr_mem;
+          GElf_Shdr* shdr = gelf_getshdr (scn, &shdr_mem);
+          size_t entsize = (shdr != NULL && shdr->sh_entsize != 0
+                            ? shdr->sh_entsize
+                            : gelf_fsize (elf, ELF_T_DYN, 1, EV_CURRENT));
+          int maxcnt = (shdr != NULL
+                        ? shdr->sh_size / entsize : INT_MAX);
+          ABG_ASSERT (shdr == NULL || (shdr->sh_type == SHT_DYNAMIC
+				       || shdr->sh_type == SHT_PROGBITS));
+          Elf_Data* data = elf_getdata (scn, NULL);
+          if (data == NULL)
+            break;
+
+          for (int cnt = 0; cnt < maxcnt; ++cnt)
+            {
+              GElf_Dyn dynmem;
+              GElf_Dyn* dyn = gelf_getdyn (data, cnt, &dynmem);
+              if (dyn == NULL)
+                continue;
+
+              if (dyn->d_tag == DT_NULL)
+                break;
+
+              if (dyn->d_tag != DT_SONAME)
+                continue;
+
+              soname = elf_strptr (elf, shdr->sh_link, dyn->d_un.d_val);
+              break;
+            }
+          break;
+        }
+    }
+
+  elf_end(elf);
+  close(fd);
+
+  return true;
+}
 
 } // end namespace elf_helpers
 } // end namespace abigail

include/Makefile.am
@@ -4,8 +4,10 @@ abg-sptr-utils.h	\
 abg-interned-str.h	\
 abg-ir.h		\
 abg-corpus.h		\
+abg-fe-iface.h		\
+abg-elf-based-reader.h  \
+abg-elf-reader.h	\
 abg-reader.h		\
-abg-elf-reader-common.h \
 abg-dwarf-reader.h	\
 abg-writer.h		\
 abg-comparison.h	\

include/abg-corpus.h
@@ -45,9 +45,10 @@ public:
   {
     ARTIFICIAL_ORIGIN = 0,
     NATIVE_XML_ORIGIN = 1,
-    DWARF_ORIGIN      = 1 << 1,
-    CTF_ORIGIN        = 1 << 2,
-    LINUX_KERNEL_BINARY_ORIGIN = 1 << 3
+    ELF_ORIGIN        = 1 << 1,
+    DWARF_ORIGIN      = 1 << 2,
+    CTF_ORIGIN        = 1 << 3,
+    LINUX_KERNEL_BINARY_ORIGIN = 1 << 4
   };
 
 private:
@@ -68,7 +69,7 @@ public:
   get_environment() const;
 
   void
-  add(const translation_unit_sptr);
+  add(const translation_unit_sptr&);
 
   const translation_units&
   get_translation_units() const;
@@ -326,10 +327,10 @@ public:
   exported_variables();
 
   void
-  maybe_add_fn_to_exported_fns(function_decl*);
+  maybe_add_fn_to_exported_fns(const function_decl*);
 
   void
-  maybe_add_var_to_exported_vars(var_decl*);
+  maybe_add_var_to_exported_vars(const var_decl*);
 }; //corpus::exported_decls_builder
 
 /// Abstraction of a group of corpora.

include/abg-ctf-reader.h
@@ -17,40 +17,24 @@
 #include <ostream>
 #include "abg-corpus.h"
 #include "abg-suppression.h"
-#include "abg-elf-reader-common.h"
+#include "abg-elf-based-reader.h"
 
 #include "ctf-api.h"
 
 namespace abigail
 {
-namespace ctf_reader
+namespace ctf
 {
 
-class read_context;
-typedef shared_ptr<read_context> read_context_sptr;
-
-read_context_sptr
-create_read_context(const std::string& elf_path,
-                    const vector<char**>& debug_info_root_paths,
-                    ir::environment& env);
-corpus_sptr
-read_corpus(read_context *ctxt, elf_reader::status& status);
-
-corpus_sptr
-read_corpus(const read_context_sptr &ctxt, elf_reader::status &status);
-
-corpus_sptr
-read_and_add_corpus_to_group_from_elf(read_context*, corpus_group&, elf_reader::status&);
+elf_based_reader_sptr
+create_reader(const std::string& elf_path,
+	      const vector<char**>& debug_info_root_paths,
+	      environment& env);
 
 void
-set_read_context_corpus_group(read_context& ctxt, corpus_group_sptr& group);
-
-void
-reset_read_context(read_context_sptr &ctxt,
-                   const std::string&	elf_path,
+reset_reader(elf_based_reader&		ctxt,
+	     const std::string&	elf_path,
 	     const vector<char**>&	debug_info_root_path);
-std::string
-dic_type_key(ctf_dict_t *dic, ctf_id_t ctf_type);
 } // end namespace ctf_reader
 } // end namespace abigail
 

include/abg-dwarf-reader.h
@@ -18,74 +18,37 @@
 #include <elfutils/libdwfl.h>
 #include "abg-corpus.h"
 #include "abg-suppression.h"
-#include "abg-elf-reader-common.h"
+#include "abg-elf-based-reader.h"
 
 namespace abigail
 {
 
 /// The namespace for the DWARF reader.
-namespace dwarf_reader
+namespace dwarf
 {
 
 using namespace abigail::ir;
 
-/// The kind of ELF file we are looking at.
-enum elf_type
-{
-  /// A normal executable binary
-  ELF_TYPE_EXEC,
-  /// A Position Independant Executable binary
-  ELF_TYPE_PI_EXEC,
-  /// A dynamic shared object, a.k.a shared library binrary.
-  ELF_TYPE_DSO,
-  /// A relocatalbe binary.
-  ELF_TYPE_RELOCATABLE,
-  /// An unknown kind of binary.
-  ELF_TYPE_UNKNOWN
-};
-
-class read_context;
-
-/// A convenience typedef for a smart pointer to a
-/// dwarf_reader::read_context.
-typedef shared_ptr<read_context> read_context_sptr;
-
-read_context_sptr
-create_read_context(const std::string&	elf_path,
-		    const vector<char**>& debug_info_root_paths,
-		    ir::environment&	environment,
-		    bool		read_all_types = false,
-		    bool		linux_kernel_mode = false);
-
-const string&
-read_context_get_path(const read_context&);
+elf_based_reader_sptr
+create_reader(const std::string&	elf_path,
+	      const vector<char**>& debug_info_root_paths,
+	      environment&	environment,
+	      bool		read_all_types = false,
+	      bool		linux_kernel_mode = false);
 
 void
-reset_read_context(read_context_sptr &ctxt,
-		   const std::string&	elf_path,
-		   const vector<char**>& debug_info_root_paths,
-		   bool		read_all_types = false,
-		   bool		linux_kernel_mode = false);
-
-void
-add_read_context_suppressions(read_context& ctxt,
-			      const suppr::suppressions_type& supprs);
-
-void
-set_read_context_corpus_group(read_context& ctxt, corpus_group_sptr& group);
+reset_reader(elf_based_reader&		rdr,
+	     const std::string&		elf_path,
+	     const vector<char**>&	debug_info_root_paths,
+	     bool			read_all_types = false,
+	     bool			linux_kernel_mode = false);
 
 corpus_sptr
-read_corpus_from_elf(read_context& ctxt, elf_reader::status& stat);
-
-corpus_sptr
-read_corpus_from_elf(const std::string& elf_path,
-		     const vector<char**>& debug_info_root_paths,
-		     ir::environment&	environment,
-		     bool		load_all_types,
-		     elf_reader::status&);
-
-corpus_sptr
-read_and_add_corpus_to_group_from_elf(read_context&, corpus_group&, elf_reader::status&);
+read_corpus_from_elf(const std::string&	elf_path,
+		     const vector<char**>&	debug_info_root_paths,
+		     environment&		environment,
+		     bool			load_all_types,
+		     fe_iface::status&		status);
 
 bool
 lookup_symbol_from_elf(const environment&		env,
@@ -99,61 +62,7 @@ lookup_public_function_symbol_from_elf(const environment&		env,
 				       const string&			path,
 				       const string&			symname,
 				       vector<elf_symbol_sptr>&	func_syms);
-
-bool
-refers_to_alt_debug_info(const read_context&	ctxt,
-			 string&		alt_di_path);
-
-elf_reader::status
-has_alt_debug_info(read_context&	ctxt,
-		   bool&		has_alt_di,
-		   string&		alt_debug_info_path);
-
-elf_reader::status
-has_alt_debug_info(const string&	elf_path,
-		   char**		debug_info_root_path,
-		   bool&		has_alt_di,
-		   string&		alt_debug_info_path);
-
-bool
-get_soname_of_elf_file(const string& path, string& soname);
-
-bool
-get_type_of_elf_file(const string& path, elf_type& type);
-
-
-void
-set_debug_info_root_path(read_context& ctxt,
-			 char** path);
-
-char**
-get_debug_info_root_path(read_context& ctxt,
-			 char**& path);
-
-bool
-get_show_stats(read_context& ctxt);
-
-void
-set_show_stats(read_context& ctxt,
-	       bool f);
-
-void
-set_drop_undefined_syms(read_context& ctxt,
-			bool f);
-
-void
-set_do_log(read_context& ctxt, bool f);
-
-void
-set_environment(read_context& ctxt,
-		ir::environment*);
-
-const environment_sptr&
-get_environment(const read_context& ctxt);
-
-environment_sptr&
-get_environment(read_context& ctxt);
-}// end namespace dwarf_reader
+}// end namespace dwarf
 
 }// end namespace abigail
 

include/abg-elf-based-reader.h
@@ -1,0 +1,63 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// -*- Mode: C++ -*-
+//
+// Copyright (C) 2022 Red Hat, Inc.
+//
+// Author: Dodji Seketeli
+
+/// @file
+///
+/// This file contains the declarations for an elf-based.  DWARF and
+/// CTF readers can inherit this one.
+
+
+#ifndef __ABG_ELF_BASED_READER_H__
+#define __ABG_ELF_BASED_READER_H__
+
+#include <memory>
+#include <string>
+
+#include "abg-elf-reader.h"
+#include "abg-corpus.h"
+
+namespace abigail
+{
+
+/// The common interface of readers based on ELF.
+///
+/// These are for readers like DWARF and CTF readers that read debug
+/// information describing binaries in the ELF format.
+///
+/// This interface extends the elf_reader::reader interface and thus
+/// also provides facilities for reading ELF binaries.
+class elf_based_reader : public elf::reader
+{
+  struct priv;
+  priv* priv_;
+
+  elf_based_reader() = delete;
+
+protected:
+
+  /// Readers that implement this interface must provide a factory
+  /// method to create a reader instance as this constructor is
+  /// protected.
+  elf_based_reader(const std::string& elf_path,
+		   const vector<char**>& debug_info_root_paths,
+		   environment& env);
+public:
+
+  ~elf_based_reader();
+
+  virtual void
+  reset(const std::string& elf_path,
+	const vector<char**>& debug_info_root_paths);
+
+  virtual ir::corpus_sptr
+  read_and_add_corpus_to_group(ir::corpus_group& group,
+			       fe_iface::status& status);
+};//end class elf_based_reader
+
+typedef std::shared_ptr<elf_based_reader> elf_based_reader_sptr;
+}// namespace
+#endif

include/abg-elf-reader-common.h
@@ -1,70 +1,0 @@
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-// -*- Mode: C++ -*-
-//
-// Copyright (C) 2021-2022 Oracle, Inc.
-//
-// Author: Jose E. Marchesi
-
-/// @file
-///
-/// This file contains declarations implementing the different status
-/// in which a corpus read from an ELF file can result.  It is used by
-/// the readers based on ELF files, such as DWARF and CTF.
-///
-/// More generally, this file contains declarations related to
-/// facilities shared by the various readers that handle the ELF
-/// format, e.g, the DWARF and CTF realder.
-
-#ifndef __ABG_ELF_READER_COMMON_H__
-#define __ABG_ELF_READER_COMMON_H__
-
-#include <string>
-
-namespace abigail
-{
-
-/// The namespace for an ELF based reader.
-namespace elf_reader
-{
-
-/// The status of the @ref read_corpus_from_elf() call.
-enum status
-{
-  /// The status is in an unknown state
-  STATUS_UNKNOWN = 0,
-
-  /// This status is for when the call went OK.
-  STATUS_OK = 1,
-
-  /// This status is for when the debug info could not be read.
-  STATUS_DEBUG_INFO_NOT_FOUND = 1 << 1,
-
-  /// This status is for when the alternate debug info could not be
-  /// found.
-  STATUS_ALT_DEBUG_INFO_NOT_FOUND = 1 << 2,
-
-  /// This status is for when the symbols of the ELF binaries could
-  /// not be read.
-  STATUS_NO_SYMBOLS_FOUND = 1 << 3,
-};
-
-std::string
-status_to_diagnostic_string(status s);
-
-status
-operator|(status, status);
-
-status
-operator&(status, status);
-
-status&
-operator|=(status&, status);
-
-status&
-operator&=(status&, status);
-
-}// end namespace elf_reader
-
-}// end namespace abigail
-
-#endif //__ABG_ELF_READER_COMMON_H__

include/abg-elf-reader.h
@@ -1,0 +1,161 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// -*- Mode: C++ -*-
+//
+// Copyright (C) 2022 Red Hat, Inc.
+//
+// Author: Dodji Seketeli
+
+/// @file
+///
+/// This file contains the declarations for the @ref fe_iface a.k.a
+/// "Front End Interface".
+
+#ifndef __ABG_ELF_READER_H__
+#define __ABG_ELF_READER_H__
+
+#include <memory>
+#include <string>
+
+#include <elfutils/libdwfl.h>
+
+#include "abg-fe-iface.h"
+#include "abg-ir.h"
+#include "abg-suppression.h"
+
+namespace abigail
+{
+
+/// The namespace for the ELF Reader.
+namespace elf
+{
+
+/// The kind of ELF file we are looking at.
+enum elf_type : unsigned
+{
+  /// A normal executable binary
+  ELF_TYPE_EXEC,
+  /// A Position Independant Executable binary
+  ELF_TYPE_PI_EXEC,
+  /// A dynamic shared object, a.k.a shared library binary.
+  ELF_TYPE_DSO,
+  /// A relocatalbe binary.
+  ELF_TYPE_RELOCATABLE,
+  /// An unknown kind of binary.
+  ELF_TYPE_UNKNOWN
+};
+
+/// This is the interface an ELF reader.
+///
+/// It knows how to open an ELF file, read its content and expose an
+/// interface for its symbol table and other properties.
+///
+/// Note that the ABI corpus returned by the elf::read_corpus()
+/// member function doesn't contain any type representation.  It only
+/// contains the representations of the the ELF symbols found in the
+/// ELF file.
+///
+/// To construct the type representations for the functions and global
+/// variables present in the ELF file, please use the implementations
+/// of the @ref elf_based_reader interface.  Those know how to read
+/// the debug information from the ELF file to build type
+/// representation in the @ref abigail::ir::corpus instance.
+class reader : public fe_iface
+{
+  struct priv;
+  priv *priv_;
+
+ public:
+
+  reader(const std::string&	elf_path,
+	 const vector<char**>&	debug_info_roots,
+	 environment&		env);
+
+  ~reader();
+
+  void
+  reset(const std::string&	elf_path,
+	 const vector<char**>&	debug_info_roots);
+
+  const vector<char**>&
+  debug_info_root_paths() const;
+
+  const Dwfl_Callbacks&
+  dwfl_offline_callbacks() const;
+
+  Dwfl_Callbacks&
+  dwfl_offline_callbacks();
+
+  Elf*
+  elf_handle() const;
+
+  const Dwarf*
+  dwarf_debug_info() const;
+
+  const Dwarf*
+  alternate_dwarf_debug_info() const;
+
+  const string&
+  alternate_dwarf_debug_info_path() const;
+
+  bool
+  refers_to_alt_debug_info(string& alt_di_path) const;
+
+  const Elf_Scn*
+  find_symbol_table_section() const;
+
+  void
+  reset_symbol_table_section();
+
+  const Elf_Scn*
+  find_ctf_section() const;
+
+  const Elf_Scn*
+  find_alternate_ctf_section() const;
+
+  const vector<string>&
+  dt_needed()const;
+
+  const string&
+  elf_architecture() const;
+
+  symtab_reader::symtab_sptr&
+  symtab() const;
+
+  elf_symbol_sptr
+  function_symbol_is_exported(GElf_Addr symbol_address) const;
+
+  elf_symbol_sptr
+  variable_symbol_is_exported(GElf_Addr symbol_address) const;
+
+  elf_symbol_sptr
+  function_symbol_is_exported(const string& name) const;
+
+  elf_symbol_sptr
+  variable_symbol_is_exported(const string& name) const;
+
+  void
+  load_dt_soname_and_needed();
+
+  void
+  load_elf_architecture();
+
+  void
+  load_elf_properties();
+
+  virtual ir::corpus_sptr
+  read_corpus(status& status);
+};//end class reader.
+
+/// A convenience typedef for a smart pointer to a
+/// elf::reader.
+typedef shared_ptr<elf::reader> reader_sptr;
+
+bool
+get_soname_of_elf_file(const string& path, string &soname);
+
+bool
+get_type_of_elf_file(const string& path, elf_type& type);
+} // end namespace elf.
+} // end namespace abigail
+
+#endif // __ABG_ELF_READER_H__

commit cc7cfc52ee3cc9fff1b674df8ecbfcd2807b4e60

elf-helpers: make sure config.h is included first
Before this change, verify-elf used to complain on x86 and armv7:
verify-elf: ERROR: ./usr/lib/libabigail.so.2.0.0: uses non-LFS functions: open

Include config.h in abg-elf-helpers.cc before other headers so that
AC_SYS_LARGEFILE applies to this file.  This fully enables LFS
in those of 32-bit systems that do not enable it by default.

	* src/abg-elf-helpers.cc: Include "config.h" first.

Fixes: 7bd69830520f ("Make Front Ends first class citizens")
Signed-off-by: Dmitry V. Levin <ldv@strace.io>

src/abg-elf-helpers.cc
@@ -6,6 +6,7 @@
 /// @file
 ///
 /// This contains the definitions of the ELF utilities for the dwarf reader.
+#include "config.h"
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>